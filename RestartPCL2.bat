648790ed3d9dc5880c00dd485ba4181a7b03ce19
HaNWorkspace <202352273@qq.com>
2024-02-20 12:18:22 +0800

    first commit

---
+++ RestartPCL2.bat
@@ -0,0 +1,3 @@
+@echo off
+taskkill /im "Plain Craft Launcher 2.exe" /f
+"D:\PDFelement\Minecraft\Launcher\Plain Craft Launcher 2.exe"
---
+++ RestartPCL2.vbs
@@ -0,0 +1,2 @@
+set ws=WScript.CreateObject("WScript.Shell")
+ws.Run "D:\PDFelement\Minecraft\Launcher\PCL\RestartPCL2.bat",0
